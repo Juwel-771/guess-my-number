9efdec029d1d2dd7b1c51fa8cd5e3d14f0678550
Juwel-771 <juwelah99@gmail.com>
2022-04-19 18:11:07 +0600

    game logic developed

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = "🎉 Correct Number";
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = "🎉 Correct Number";
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 20;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 20;
+// console.log(document.querySelector('.guess').value);
+
+const checkValue  = document.querySelector('.check');
+
+checkValue.addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+
+    if(!guess){
+        document.querySelector('.message').textContent = "🚩No Number";
+    }else{
+        document.querySelector('.message').value;
+    }
+});
