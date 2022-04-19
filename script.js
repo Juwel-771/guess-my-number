4f876b5093bf08e5dfade54285b179727dcf419b
Juwel-771 <juwelah99@gmail.com>
2022-04-19 16:49:53 +0600

    Test Case

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = "🎉 Correct Number";
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 20;
+console.log(document.querySelector('.guess').value);
